880fde01cb04aa656cbe2e3f4906ea57497513de
cloudhum <turbulentbees@gmail.com>
2022-05-01 21:48:14 -0700

    Add mouseover event listeners

--- script.js
+++ script.js
@@ -2,9 +2,18 @@ function createDivs(num){
     const container = document.querySelector(".container");
     for (let i = 1; i <= num; i++){
         const div = document.createElement('div')
-        div.classList.add('cell');
+        div.classList.add('square');
         container.appendChild(div);
     }
 }
-
 createDivs(256);
+//add eventlisteners 
+const squares = document.querySelectorAll('.square');
+console.log(squares);
+
+squares.forEach((square) => {
+    square.addEventListener('mouseover', () => {
+        square.style.backgroundColor = "black";
+    });
+});
+
