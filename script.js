a7a67dc428e6028d60b0f2638988bae90dda6294
cloudhum <turbulentbees@gmail.com>
2022-05-02 13:43:04 -0700

    Grid size changes based on user input

--- script.js
+++ script.js
@@ -1,31 +1,53 @@
-function createDivs(num){
-    const container = document.querySelector(".container");
-    for (let i = 1; i <= num; i++){
+// get container and button, add click event listener
+const container = document.querySelector(".container");
+const selectGridSizeBtn = document.querySelector('.grid-select');
+selectGridSizeBtn.addEventListener('click', capturePrompt);
+
+//create intiial grid 
+createDivs(256);
+listenToSquares();
+
+function createDivs(num) {
+    for (let i = 1; i <= num; i++) {
         const div = document.createElement('div')
         div.classList.add('square');
         container.appendChild(div);
     }
 }
-//createDivs(256);//
-createDivs(4096);
 
-//get squares
-const squares = document.querySelectorAll('.square');
-console.log(squares);
-
-//add event listeners for each square/div
-squares.forEach((square) => {
-    square.addEventListener('mouseover', () => {
-        square.style.backgroundColor = "lavender";
+function listenToSquares() {
+    //get squares and add event listeners for each square/div
+    let squares = document.querySelectorAll('.square');
+    squares.forEach((square) => {
+        square.addEventListener('mouseover', () => {
+            square.style.backgroundColor = "lavender";
+        });
     });
-});
+}
+
+function resetGrid(input) {
+    // remove all current divs
+    let squares = document.querySelectorAll('.square');
+    squares.forEach((square) => {
+        container.removeChild(square);
+    });
+
+    // create new divs/squares
+    createDivs(input * input);
+
+    //change div height/width
+    squares = document.querySelectorAll('.square');
+    squares.forEach((square) => {
+        square.style.width = `${960 / input}px`;
+        square.style.height = `${960 / input}px`;
+    });
+
+    //attach event listeners to new divs
+    listenToSquares();
+}
 
 function capturePrompt() {
     let input = prompt('Enter grid size:');
     console.log(input);
+    resetGrid(input);
 }
-
-//grab button and add event listener
-const selectGridSizeBtn = document.querySelector('.grid-select');
-selectGridSizeBtn.addEventListener('click', capturePrompt);
-
